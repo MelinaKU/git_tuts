66bd6d5767dd44c336426d1daa6d7ba5da576d56
[Melina Singh] <[melinacingh@gmail.com]>
2022-12-06 13:02:32 +0545

    New modified Code

--- hello.c
+++ hello.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 int main(){
-    printf("Hello i am learnng to use git today .");
+    printf("Hello i am learnng to use git today mmmmm .");
     printf("Lets see what  happens today");
+    printf("lets see the difference");
+    printf("Gitt Tution");
     return 0;
 }
